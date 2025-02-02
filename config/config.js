3081959a245b0d49d96b939edea1d6f60263892b
alexanderbry <alexunderbrain@gmail.com>
2025-02-02 21:46:29 +0700

    refactor: deployment config

--- config/config.js
+++ config/config.js
@@ -2,19 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + "_dev",
+    username: process.env.PGUSERNAME,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE + "_dev",
     host: 'localhost',
     dialect: "postgres",
     logging: false
   },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.HOST,
-    dialect: "postgres",
-    logging: false
-  },
+  "production": {
+    "username": "neondb_owner",
+    "password": "npg_H7XnpOArYkt0",
+    "database": "neondb",
+    "host": "ep-wispy-mud-a1o0zzj4-pooler.ap-southeast-1.aws.neon.tech",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  }
 };
